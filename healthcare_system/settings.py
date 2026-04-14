6bd84fac7ee64985b1ff65502426359280168f54
temporalzone <temporalzone9@gmail.com>
2026-04-14 18:13:35 +0530

    Settings: Configured python-dotenv for local development without exposing credentials

--- healthcare_system/settings.py
+++ healthcare_system/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import os
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
